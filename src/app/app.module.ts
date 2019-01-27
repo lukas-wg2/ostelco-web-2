832d79604228ef40ba2fa20068f9523c69be910c
havard <havardnoren@gmail.com>
2019-01-27 15:38:09 +0100

    fix: fail to build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     IonicModule.forRoot(),
     AppRoutingModule
   ],
